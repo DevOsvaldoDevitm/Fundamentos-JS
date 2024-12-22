50151c1f7b0bd5557dfe82a421bc5d6d475e98fc
DevOsvaldoDevitm <osvaldo.maye@devitm.com>
2024-12-22 17:32:04 -0600

    Callbacks hell

--- callback-hell.js
+++ callback-hell.js
@@ -25,7 +25,7 @@ const salarios = [
 ];
 
 const getEmpleado = ( id, callback ) =>{
-    const empleado = empleados.find( e => e.id === id) 
+    const empleado = empleados.find( e => e.id === id) ?.nombre
 
     if (empleado){
         
@@ -35,17 +35,33 @@ const getEmpleado = ( id, callback ) =>{
         callback (`Empleado con id ${id} no existe`);
     }
 
-
 }
 
-getEmpleado(3, (err, empleado)=>{
+const getSalario = (id, callback) => {
+    const salario = salarios.find(s => s.id === id)?.salario;
+    if(salario){
+        callback (null, salario);
+    }else{
+        callback(`El salario del usuario con id ${id} no cuenta con fondos`)
+    }
+};
+const id = 1;
+
+getEmpleado(id, (err, empleado)=>{
 
     if (err){
         console.log('ERROR!');
         return console.log(err);
     }
-    console.log('Empleado en existencia');
-    console.log(empleado);
+    
+    getSalario(id, (err, salario)=>{
+        if(err){
+            return console.log(err);
+        }
+        
+        console.log('El empleado:', empleado, 'tiene un salario de: ',salario);
+    })
 })
+
 
 // console.log (getEmpleado (4) );
